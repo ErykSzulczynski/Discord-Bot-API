79ee4d7fe32d372d97355438b74209b7682db274
ErykSzulczynski <eryk.szulczynski0@gmail.com>
2021-03-24 10:02:02 +0100

    Database connection

--- index.js
+++ index.js
@@ -5,5 +5,15 @@ require('dotenv').config();
 
 const app = express()
 const bcrypt = require('bcrypt')
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+})
+
+connection.connect()
 
 app.listen(process.env.API_PORT, () => {console.log("API started on port: " + process.env.API_PORT)})
